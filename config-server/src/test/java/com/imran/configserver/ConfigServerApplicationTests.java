01cc2589e41280157b0e69d558f4845cab40bfe9
mohammed imran <mohammedimran1939543@gmail.com>
2023-03-25 22:44:31 +0530

    Cloud-config-server

---
+++ config-server/src/test/java/com/imran/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imran.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
